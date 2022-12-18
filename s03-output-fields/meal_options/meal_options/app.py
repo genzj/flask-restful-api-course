14ebeea0ebe4295e11c43081718db0cb4a06159b
genzj <zj0512@gmail.com>
2022-12-17 22:45:02 -0800

    update s03

--- s03-output-fields/meal_options/meal_options/app.py
+++ s03-output-fields/meal_options/meal_options/app.py
@@ -1,13 +1,22 @@
+from . import create_app
+from .model import Option, Meal, TIME_FORMAT, list_of
+
 import json
 
 from datetime import datetime
 from typing import List
 
 from flask import request
-from flask_restful import Resource, marshal_with, marshal_with_field, fields
+from flask_restful import (
+    Api,
+    Resource,
+    fields,
+    marshal_with,
+    marshal_with_field,
+)
 
-from . import api
-from .model import Option, Meal, TIME_FORMAT, list_of
+app = create_app()
+api = Api(app)
 
 meals: List[Meal] = []
 
@@ -29,7 +38,6 @@ def create_option(meal_id, place):
 
 
 class Meals(Resource):
-
     @marshal_with_field(list_of(Meal))
     def get(self):
         return meals
@@ -44,7 +52,6 @@ api.add_resource(Meals, '/meals')
 
 
 class Options(Resource):
-
     @marshal_with_field(list_of(Option))
     def get(self, meal_id):
         return meals[meal_id].options
@@ -59,7 +66,6 @@ api.add_resource(Options, '/meals/<int:meal_id>/options')
 
 
 class Votes(Resource):
-
     @marshal_with_field(fields.Integer)
     def post(self, meal_id, option_id):
         meals[meal_id].options[option_id].votes += 1
