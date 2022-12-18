c117d0053bfe807eb085b6a537119e2c7463560b
genzj <zj0512@gmail.com>
2022-12-18 12:32:34 -0800

    update S07

--- s07-blueprint/meal_options/meal_options/auth.py
+++ s07-blueprint/meal_options/meal_options/auth.py
@@ -1,27 +1,17 @@
-from flask import request, current_app, _request_ctx_stack
+from flask import request, current_app
 from flask_restful import abort
-from werkzeug.local import LocalProxy
-
-current_user = LocalProxy(lambda: _request_ctx_stack.top.user)
 
 
 def login():
-    if 'USER_TOKEN' in request.headers and \
-            request.headers['USER_TOKEN'] == 'test-user':
-        _request_ctx_stack.top.user = 'test'
+    token_map = current_app.config.get('TOKEN_TO_USER_MAPPING', dict())
+    if 'USER_TOKEN' in request.headers:
+        current_user = token_map.get(request.headers['USER_TOKEN'], None)
+    else:
+        current_user = None
+
+    if current_user:
         current_app.logger.info('user %s login', current_user)
     else:
         current_app.logger.warning('no user login')
-        _request_ctx_stack.top.user = None
-
-    return _request_ctx_stack.top.user is not None
-
-
-def login_required(f):
-
-    def _wrapped(*args, **kwargs):
-        if not login():
-            abort(401)
-        return f(*args, **kwargs)
-
-    return _wrapped
+        abort(403)
+    return current_user
